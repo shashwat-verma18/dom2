2d71e6ff5063d9a51344bd06d4fa3eceb7d51001
Shashwat Verma <shashwatv18@gmail.com>
2023-12-04 03:50:54 +0530

    querySelectorAll

--- dom.js
+++ dom.js
@@ -1,5 +1,14 @@
-var item2 = document.querySelector('.list-group-item:nth-child(2)');
-item2.style.background = 'green';
+// var item2 = document.querySelector('.list-group-item:nth-child(2)');
+// item2.style.background = 'green';
 
-var item3 = document.querySelector('.list-group-item:nth-child(3)');
-item3.style.visibility = 'hidden';
+// var item3 = document.querySelector('.list-group-item:nth-child(3)');
+// item3.style.visibility = 'hidden';
+
+
+var items = document.querySelectorAll('li');
+items[1].style.color = 'green';
+
+var items = document.querySelectorAll('li:nth-child(odd)');
+
+for(item of items)
+    item.style.background = 'green';
