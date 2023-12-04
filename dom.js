c202504653507062aec7f975e02c615b83b4e451
Shashwat Verma <shashwatv18@gmail.com>
2023-12-04 12:17:54 +0530

    dom Manipulate

--- dom.js
+++ dom.js
@@ -1,14 +1,64 @@
-// var item2 = document.querySelector('.list-group-item:nth-child(2)');
-// item2.style.background = 'green';
+var itemList = document.querySelector('#items');
 
-// var item3 = document.querySelector('.list-group-item:nth-child(3)');
-// item3.style.visibility = 'hidden';
+// parentElement 
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+
+// lastElementChild
+itemList.lastElementChild.textContent = 'Hello 4';
+
+// lastChild
+console.log(itemList.lastChild);
+
+// createChild
+
+//firstElementChild
+itemList.firstElementChild.textContent = 'Hello 1';
+
+// firstChild 
+console.log(itemList.firstChild);
+
+// nextSibling
+console.log(itemList.nextSibling);
+
+// nextElementSibling
+console.log(itemList.nextElementSibling);  //null
+
+// prviousSibling
+console.log(itemList.previousSibling);
+
+// previousElementSibling
+itemList.previousElementSibling.style.fontWeight = 'bold';
+
+// createElement
+var newDiv = document.createElement('div');
+newDiv.className= 'hello';
+newDiv.id = 'hellol';
+
+// setAttribute
+newDiv.setAttribute('title', 'Hello Div');
+
+// createsNode
+var newDivText = document.createTextNode ( 'Hello World');
+
+//appendChild
+newDiv.appendChild(newDivText);
+
+newDiv.style.fontSize = '30px';
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector ('header h1');
+container.insertBefore(newDiv, h1);
+
+var newDiv2 = document.createElement('div2');
+newDiv2.className= 'list-group-item';
+newDiv2.id = 'hello2';
+
+var newDivText2 = document.createTextNode ( 'Hello World');
+
+newDiv2.appendChild(newDivText2);
+
+var container2 = document.getElementById('items');
+var item1 = document.querySelector('.list-group-item:first-child');
+container2.insertBefore(newDiv2, item1);
 
 
-var items = document.querySelectorAll('li');
-items[1].style.color = 'green';
-
-var items = document.querySelectorAll('li:nth-child(odd)');
-
-for(item of items)
-    item.style.background = 'green';
